c432fdd1d5c7d4e2b2789396886a3eff699f7fd5
Jon Nightingale <jnightin@cisco.com>
2021-12-02 10:11:55 +0000

    Tidy-up for per-day files

---
+++ Sources/advent/day1.swift
@@ -0,0 +1,37 @@
+import Foundation
+
+/// Load the whole file, split into lines, convert to Int
+func loadIntsFromFile() -> [Int?]? {
+    return try? String(contentsOfFile: "./inputs/day1/input.txt", encoding: String.Encoding.ascii)
+                        .split{$0 == "\n"}
+                        .map(String.init)
+                        .map(Int.init)
+}
+
+func part1() {
+    let lines = loadIntsFromFile()
+
+    // Use zip to create a list of tuples of consecutive numbers, compare them, 
+    // and set 1 for the case we care about.
+    let diffs = zip(lines!, lines!.dropFirst()).map {($0.0! < $0.1! ? 1 : 0)}
+
+    // Print the total number of cases found.
+    print(diffs.reduce(0,+))
+}
+
+func part2() {
+    let lines = loadIntsFromFile()
+
+    // Use zip to create a list of tuples of consecutive numbers, compare them, 
+    // and set 1 for the case we care about.
+    let diffs2 = zip(lines!, zip(lines!.dropFirst(), lines!.dropFirst(2))).map {$0.0! + $0.1.0! + $0.1.1!}
+    let diffs = zip(diffs2, diffs2.dropFirst()).map {($0.0 < $0.1 ? 1 : 0)}
+
+    // Print the total number of cases found.
+    print(diffs.reduce(0,+))
+}
+
+func day1() {
+    part1()
+    part2()
+}
--- Sources/advent/main.swift
+++ Sources/advent/main.swift
@@ -1,9 +1,1 @@
-import Foundation
-
-let lines = try? String(contentsOfFile: "./input.txt", encoding: String.Encoding.ascii)
-                     .split{$0 == "\n"}
-                     .map(String.init)
-                     .map(Int.init)
-
-let diffs = zip(lines!, lines!.dropFirst()).map {($0.0! < $0.1! ? 1 : 0)}
-print(diffs.reduce(0,+))
+day1()
